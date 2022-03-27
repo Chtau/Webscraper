6c4252da433ef9d75764f4823b58a26508237640
Chtau <taucher.ch@gmail.com>
2022-03-27 22:57:25 +0200

    basic regex search for anchor elements

--- src/scraper/scraperengine.cpp
+++ src/scraper/scraperengine.cpp
@@ -1,3 +1,5 @@
+#include <regex>
+
 #include "scraperengine.h"
 
 #include <curlpp/cURLpp.hpp>
@@ -13,7 +15,6 @@ namespace Scraper
 
     ScraperEngine::ScraperEngine()
     {
-        
     }
 
     ScraperEngine::~ScraperEngine()
@@ -26,8 +27,27 @@ namespace Scraper
 
         HTTPDownloader downloader;
         std::string content = downloader.download(url);
-        std::cout << content << std::endl;
-        
+        std::regex expression("<a[^>]+href=\"(.*?)\"[^>]*>(.*)?</a>");
+        std::string word;
+        std::stringstream ss(content);
+        std::vector<std::string> urls;
+        std::smatch sm;
+
+        while (std::regex_search(content, sm, expression))
+        {
+            urls.push_back(sm.str());
+            content = sm.suffix();
+        }
+
+        std::cout << "Urls found: " << urls.size() << std::endl;
+
+        if (urls.size() > 0)
+        {
+            for (size_t i = 0; i < urls.size(); i++)
+            {
+                std::cout << "Url:" << urls[i] << std::endl;
+            }
+        }
     }
 
 }
