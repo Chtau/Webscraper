d41549722b8f506e583278cf74f988238ebad55b
Chtau <taucher.ch@gmail.com>
2022-04-01 15:08:03 +0200

    add argument for depth level

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,23 @@
 int main(int argc, char *argv[])
 {
     if (argc >= 2) {
+        int depth_level = 0;
+        std::vector<std::string> arguments {};
+        for (size_t i = 0; i < argc; i++)
+        {
+            const std::string value = argv[i];
+            arguments.push_back(value);
+            auto comp = value.compare("-l=");
+            if (comp == value.size() - 3) {
+                depth_level = std::stoi(value.substr(3, value.size() - 3));
+                std::cout << "Argument for Depth level:" << depth_level << std::endl;        
+            }
+        }
+        
+
         const std::string url = argv[1];
         Scraper::ScraperEngine engine { };
-        engine.Load(url, 1);
+        engine.Load(url, depth_level);
 
         auto links = engine.GetLinks();
         std::cout << "Total Urls discovered:" << links.size() << std::endl;
